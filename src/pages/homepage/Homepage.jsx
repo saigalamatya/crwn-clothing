46a6832fb66b64d965c802a108e0bfca218fd9fe
saigal_amatya <saigal@noveltytechnology.com>
2020-08-24 09:36:17 +0545

    homepage and styling menu items

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Directory from "../../components/Directory/Directory";
+import "./Homepage.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
