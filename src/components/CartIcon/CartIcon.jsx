f6f50b0b26d4038ad534cdc41ac8cf06a6a24a3a
saigal_amatya <saigal@noveltytechnology.com>
2020-08-28 15:47:08 +0545

    created cart-item component and connected our cart dropdown with reducer

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -6,15 +6,21 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./CartIcon.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartItems }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">
+      {cartItems && cartItems.length > 0 ? cartItems.length : 0}
+    </span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
