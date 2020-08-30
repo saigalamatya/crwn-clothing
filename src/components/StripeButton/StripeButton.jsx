59b337d353e2f296682945c6ba54080597f87d78
saigal_amatya <saigal@noveltytechnology.com>
2020-08-30 19:27:49 +0545

    updatinf stripe integration

---
+++ src/components/StripeButton/StripeButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HLqS7FgmevdY9NMukOlzoFOnGLfxZohCUTDrF0lUN77qMMeqe3u9oKAzKsWSO5vbmcu17f77Gf0fdyBXr4AnDbY00uFIfTV6h";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
